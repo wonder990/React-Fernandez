a0255343e78194234fa305a5723291b4f97abd2d
wonder990 <gustavofernandezcalizaya@gmail.com>
2022-04-27 17:06:21 -0400

    guardar

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,7 +1,7 @@
 const CartWidget = () => {
     return (
         <>
-            <a className="mx-10 my-5" href="#">
+            <a className="flex items-center mx-10 my-5" href="#">
                 <i class="fa-solid fa-cart-shopping"></i>
             </a>
         </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,12 @@ import CartWidget from "./CartWidget"
 
 const NavBar = (props) => {
     return (
-        <div className="bg-slate-400 flex justify-between">
-            <a className="text-white text-xl font-bold mx-10 my-5" href="">AGON</a>
-            <ul className="flex">
-                <li className="text-white font-medium mx-10 my-5"><a href="#">Accesorios</a></li>
-                <li className="text-white font-medium mx-10 my-5"><a href="#">Sobre Nosotros</a></li>
-                <li className="text-white font-medium mx-10 my-5"><a href="#">Descuentos</a></li>
+        <div className="bg-slate-400 flex justify-between content-center h-12">
+            <a className="text-white text-xl font-bold mx-10 flex items-center" href="">AGON</a>
+            <ul className="flex items-center">
+                <li className="text-white font-medium mx-10"><a href="#">Accesorios</a></li>
+                <li className="text-white font-medium mx-10"><a href="#">Sobre Nosotros</a></li>
+                <li className="text-white font-medium mx-10"><a href="#">Descuentos</a></li>
             </ul>
             {props.children}
         </div>
