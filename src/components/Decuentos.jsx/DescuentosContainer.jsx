5ff4a48c48dbd0dceea8243c4a2194b758f6f93a
wonder990 <gustavofernandezcalizaya@gmail.com>
2022-05-10 01:23:47 -0400

    committ

--- src/components/Decuentos.jsx/DescuentosContainer.jsx
+++ src/components/Decuentos.jsx/DescuentosContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DescuentosContainer = () => {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. In explicabo nihil vel? Quis fugit sit exercitationem similique earum voluptatem autem nesciunt ipsum voluptate ducimus! Ipsa dolorem harum maxime debitis commodi.</div>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis hic quasi laborum, nulla est delectus aspernatur quod non similique maiores dolore ut voluptate reiciendis? Sapiente beatae aliquam possimus ex amet! Officia tempore recusandae illum fugiat eius ipsum alias autem, porro odio veniam perferendis sit quia nulla assumenda aspernatur, minus natus?</div>
   )
 }
 
